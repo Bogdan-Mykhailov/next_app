cd471fb280508afef31bafb05b8afa62531c3619
Bogdan <bogdan_mykhailov@icloud.com>
2023-08-15 12:38:56 +0300

    :construction: add mongodb

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,15 +1,23 @@
-function handler(req, res) {
+import {MongoClient} from 'mongodb';
+
+async function handler(req, res) {
   if (req.method === 'POST') {
     const userEmail = req.body.email;
 
     if (!userEmail || !userEmail.includes('@')) {
-      res.status(422).json({ message: 'Invalid email address.'})
+      res.status(422).json({message: 'Invalid email address.'})
 
       return;
     }
 
-    console.log(userEmail);
-    res.status(201).json({ message: 'Signed up!' })
+    const client = await MongoClient.connect('mongodb+srv://bogdan_mykhailov:TfZnqFASlkvaEyTZ@cluster0.nsvvfdc.mongodb.net/?retryWrites=true&w=majority')
+    const db = client.db('majestic');
+
+    await db.collection('emails').insertOne({ email: userEmail });
+
+    await client.close();
+
+    res.status(201).json({message: 'Signed up!'})
   }
 }
 

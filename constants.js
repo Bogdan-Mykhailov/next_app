999951e4690a0fdf159e053d4b343de43303170c
Bogdan <bogdan_mykhailov@icloud.com>
2023-08-17 11:35:47 +0300

    :construction: fix

--- constants.js
+++ constants.js
@@ -1,4 +1,4 @@
-export const MONGODB_URL = 'mongodb+srv://bogdan_mykhailov:TfZnqFASlkvaEyTZ@cluster0.nsvvfdc.mongodb.net/?retryWrites=true&w=majority';
+export const MONGODB_URL = 'mongodb+srv://bogdan_mykhailov:yDa0UDAogCUSdBq1@cluster0.nsvvfdc.mongodb.net/?retryWrites=true&w=majority';
 export const DB_NAME = 'majestic';
 export const COLLECTIONS = {
   EMAILS: 'emails',
